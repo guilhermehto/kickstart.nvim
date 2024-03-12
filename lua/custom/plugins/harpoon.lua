9e61718c1a18fb4d6d21d998732bfe0f957ec19a
Guilherme Oliveira <htoguilherme@gmail.com>
2024-03-12 22:36:44 +1300

    feat: add harpoon

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,63 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {},
+  config = function()
+    local harpoon = require 'harpoon'
+    harpoon:setup {}
+  end,
+  keys = {
+    {
+      '<leader>ha',
+      function()
+        require('harpoon'):list():append()
+      end,
+      desc = '[H]arpoon [A]dd',
+    },
+    {
+      '<C-e>',
+      function()
+        local harpoon = require 'harpoon'
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = '[F]ind [H]arpoon',
+    },
+    {
+      '<leader>n',
+      function()
+        require('harpoon'):list():next()
+      end,
+    },
+    {
+      '<leader>p',
+      function()
+        require('harpoon'):list():prev()
+      end,
+    },
+    {
+      '<leader>1',
+      function()
+        require('harpoon'):list():select(1)
+      end,
+    },
+    {
+      '<leader>2',
+      function()
+        require('harpoon'):list():select(2)
+      end,
+    },
+    {
+      '<leader>3',
+      function()
+        require('harpoon'):list():select(3)
+      end,
+    },
+    {
+      '<leader>4',
+      function()
+        require('harpoon'):list():select(4)
+      end,
+    },
+  },
+}
