d144491f12535c41b598b695d9d189d79d82f05f
Guilherme Oliveira <htoguilherme@gmail.com>
2024-04-02 08:59:14 +1300

    feat: add signature help to cmp

--- init.lua
+++ init.lua
@@ -675,6 +675,7 @@ require('lazy').setup {
       --  nvim-cmp does not ship with all sources by default. They are split
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
       'hrsh7th/cmp-path',
     },
     config = function()
@@ -741,6 +742,7 @@ require('lazy').setup {
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'nvim_lsp_signature_help' },
         },
       }
     end,
