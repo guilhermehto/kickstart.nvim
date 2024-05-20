156235793e38269c4b6c22e3dbbcd05f3cc9d8e2
Guilherme Oliveira <htoguilherme@gmail.com>
2024-05-20 21:48:18 +1200

    fix: remove annoying lint warning about no config found

--- lua/custom/plugins/lint.lua
+++ lua/custom/plugins/lint.lua
@@ -1,11 +1,25 @@
 return {
   'mfussenegger/nvim-lint',
   config = function(_, __)
-    require('lint').linters_by_ft = {
+    local lint = require 'lint'
+    lint.linters_by_ft = {
       typescript = { 'eslint_d' },
       typescriptreact = { 'eslint_d' },
       javascript = { 'eslint_d' },
       javascriptreact = { 'eslint_d' },
+    }
+
+    local eslint = lint.linters.eslint_d
+
+    eslint.args = {
+      '--no-warn-ignored',
+      '--format',
+      'json',
+      '--stdin',
+      '--stdin-filename',
+      function()
+        return vim.api.nvim_buf_get_name(0)
+      end,
     }
 
     vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
