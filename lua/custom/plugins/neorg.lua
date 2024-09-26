45ffb6a3db6a2f32f7dfe24318df21ce0b6da2b9
Guilherme Oliveira <goliveira@atlassian.com>
2024-09-27 11:53:23 +1200

    feat: disable neorg

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -1,35 +1,36 @@
-return {
-  {
-    'vhyrro/luarocks.nvim',
-    priority = 1000,
-    config = true,
-  },
-  {
-    'nvim-neorg/neorg',
-    dependencies = { 'luarocks.nvim', 'folke/zen-mode.nvim' },
-    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-    version = '*', -- Pin Neorg to the latest stable release
-    config = function()
-      require('neorg').setup {
-        load = {
-          ['core.defaults'] = {},
-          ['core.concealer'] = {},
-          ['core.mode'] = {},
-          ['core.integrations.treesitter'] = {},
-          ['core.queries.native'] = {},
-          ['core.ui'] = {},
-          ['core.presenter'] = {
-            config = {
-              zen_mode = 'zen-mode',
-            },
-          },
-          ['core.export'] = {},
-          ['core.export.markdown'] = {},
-        },
-      }
-
-      vim.wo.foldlevel = 99
-      vim.wo.conceallevel = 2
-    end,
-  },
-}
+-- return {
+--   {
+--     'vhyrro/luarocks.nvim',
+--     priority = 1000,
+--     config = true,
+--   },
+--   {
+--     'nvim-neorg/neorg',
+--     dependencies = { 'luarocks.nvim', 'folke/zen-mode.nvim' },
+--     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+--     version = '*', -- Pin Neorg to the latest stable release
+--     config = function()
+--       require('neorg').setup {
+--         load = {
+--           ['core.defaults'] = {},
+--           ['core.concealer'] = {},
+--           ['core.mode'] = {},
+--           ['core.integrations.treesitter'] = {},
+--           ['core.queries.native'] = {},
+--           ['core.ui'] = {},
+--           ['core.presenter'] = {
+--             config = {
+--               zen_mode = 'zen-mode',
+--             },
+--           },
+--           ['core.export'] = {},
+--           ['core.export.markdown'] = {},
+--         },
+--       }
+--
+--       vim.wo.foldlevel = 99
+--       vim.wo.conceallevel = 2
+--     end,
+--   },
+-- }
+return {}
